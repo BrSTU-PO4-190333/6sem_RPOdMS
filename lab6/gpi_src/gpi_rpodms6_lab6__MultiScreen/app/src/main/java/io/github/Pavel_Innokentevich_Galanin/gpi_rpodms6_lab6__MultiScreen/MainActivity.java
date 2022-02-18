458e8e96226983d923f76c5843aa2b87f2fc95b5
Pavel-Innokentevich-Galanin <Pavel.Innokentevich.Galanin@gmail.com>
2022-02-18 16:39:02 +0300

    lab6/Galanin_Pavel : Added ArrayAdapter

--- lab6/gpi_src/gpi_rpodms6_lab6__MultiScreen/app/src/main/java/io/github/Pavel_Innokentevich_Galanin/gpi_rpodms6_lab6__MultiScreen/MainActivity.java
+++ lab6/gpi_src/gpi_rpodms6_lab6__MultiScreen/app/src/main/java/io/github/Pavel_Innokentevich_Galanin/gpi_rpodms6_lab6__MultiScreen/MainActivity.java
@@ -1,13 +1,22 @@
 package io.github.Pavel_Innokentevich_Galanin.gpi_rpodms6_lab6__MultiScreen;
 
 import android.app.ListActivity;
-
+import android.widget.ArrayAdapter;
 import android.os.Bundle;
 
 public class MainActivity extends ListActivity {
+    private ArrayAdapter<String> adapter;
+    private String[] islands = {
+            "Канары",
+            "Курилы",
+            "Мальдивы",
+            "Филиппины",
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, islands);
+        setListAdapter(adapter);
     }
 }
