bbc37826027aa11e487888877bd877fb15121ade
Pavel-Innokentevich-Galanin <Pavel.Innokentevich.Galanin@gmail.com>
2022-02-18 16:45:58 +0300

    lab6/Galanin_Pavel : Added AdapterView

--- lab6/gpi_src/gpi_rpodms6_lab6__MultiScreen/app/src/main/java/io/github/Pavel_Innokentevich_Galanin/gpi_rpodms6_lab6__MultiScreen/MainActivity.java
+++ lab6/gpi_src/gpi_rpodms6_lab6__MultiScreen/app/src/main/java/io/github/Pavel_Innokentevich_Galanin/gpi_rpodms6_lab6__MultiScreen/MainActivity.java
@@ -1,6 +1,8 @@
 package io.github.Pavel_Innokentevich_Galanin.gpi_rpodms6_lab6__MultiScreen;
 
 import android.app.ListActivity;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.os.Bundle;
 
@@ -18,5 +20,11 @@ public class MainActivity extends ListActivity {
         super.onCreate(savedInstanceState);
         adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, islands);
         setListAdapter(adapter);
+        AdapterView.OnItemClickListener itemListener = new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> arg0, View arg1, int arg2, long arg3) {
+                // TODO Auto-generated method stub
+            }
+        };
     }
 }
