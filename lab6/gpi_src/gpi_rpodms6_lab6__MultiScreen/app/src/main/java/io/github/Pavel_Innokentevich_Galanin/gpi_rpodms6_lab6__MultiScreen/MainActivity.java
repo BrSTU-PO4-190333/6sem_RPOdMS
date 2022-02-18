368f5f7a4a3b85f1a2ee18ad11d72907ee2db9a5
Pavel-Innokentevich-Galanin <Pavel.Innokentevich.Galanin@gmail.com>
2022-02-18 16:32:33 +0300

    lab6/Galanin_Pavel : Activity to ListActivity

--- lab6/gpi_src/gpi_rpodms6_lab6__MultiScreen/app/src/main/java/io/github/Pavel_Innokentevich_Galanin/gpi_rpodms6_lab6__MultiScreen/MainActivity.java
+++ lab6/gpi_src/gpi_rpodms6_lab6__MultiScreen/app/src/main/java/io/github/Pavel_Innokentevich_Galanin/gpi_rpodms6_lab6__MultiScreen/MainActivity.java
@@ -1,14 +1,13 @@
 package io.github.Pavel_Innokentevich_Galanin.gpi_rpodms6_lab6__MultiScreen;
 
-import androidx.appcompat.app.AppCompatActivity;
+import android.app.ListActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends ListActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
     }
 }
