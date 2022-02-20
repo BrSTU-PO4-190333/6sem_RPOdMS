cf06fc640b43655f218d452d3cd01efd3817d3ee
Pavel-Innokentevich-Galanin <Pavel.Innokentevich.Galanin@gmail.com>
2022-02-20 20:36:07 +0300

    lab7/Galanin_Pavel : Added empty project

---
+++ lab7/gpi_src/gpi_rpodms6_lab7__gestsubset/app/src/main/java/io/github/Pavel_Innokentevich_Galanin/gpi_rpodms6_lab7__GestSubset/MainActivity.java
@@ -0,0 +1,14 @@
+package io.github.Pavel_Innokentevich_Galanin.gpi_rpodms6_lab7__GestSubset;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
