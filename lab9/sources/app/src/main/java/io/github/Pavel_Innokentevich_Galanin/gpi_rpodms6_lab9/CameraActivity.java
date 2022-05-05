2e20f8d2b25f9cf704e05c628932b51649dba518
Pavel-Innokentevich-Galanin <Pavel.Innokentevich.Galanin@gmail.com>
2022-05-05 05:28:15 +0300

    lab9/Galanin_Pavel : Added activities

---
+++ lab9/sources/app/src/main/java/io/github/Pavel_Innokentevich_Galanin/gpi_rpodms6_lab9/CameraActivity.java
@@ -0,0 +1,12 @@
+package io.github.Pavel_Innokentevich_Galanin.gpi_rpodms6_lab9;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class CameraActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_camera);
+    }
+}
